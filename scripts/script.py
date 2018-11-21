3b320d23ad57b562f4c07c08b86ffe12ef974263
panmrherobrine <polomarket73@gmail.com>
2018-11-21 15:26:27 +0100

    skrypt.py - aktualizacja

--- scripts/script.py
+++ scripts/script.py
@@ -4,7 +4,7 @@ from time import sleep
 sense = SenseHat()
 sense.set_rotation(270)
 
-temp = round( sense.get_temperature(), 1 )
+
 
 w = (255,255,255)
 b = (0,0,0)
@@ -24,3 +24,9 @@ picture = [
 sense.show_message("GREETINGS FROM POLAND", scroll_speed=0.07, text_colour=r)
 sleep(2)
 sense.set_pixels(picture)
+
+
+
+//temperatura - Oskar
+
+temp = round( sense.get_temperature(), 1 )
