c7a0796f07e4957542e660176e1a280a7670900d
MrMagicPL <44531255+MrMagicPL@users.noreply.github.com>
2018-12-05 15:07:40 +0100

    utworzono normal temp

---
+++ normalnytemp.py
@@ -0,0 +1,17 @@
+b = (0,0,0)
+u = (0,162,232) # niebieski
+g = (0,138,0) #jasny zielony
+d = (2,70,0) #ciemny zielony
+o = (124,78,52) #brazowy
+
+
+to38 = [
+    u, u, u, u, u, u, u, u,
+    u, u, u, u, d, u, u, u,
+    u, u, u, d, d, d, u, u,
+    u, u, u, u, o, u, u, u,
+    u, u, u, u, o, u, u, u,
+    u, u, g, g, g, g, g, u,
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g
+]
