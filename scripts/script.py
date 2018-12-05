26a88e8520fbd3af9b5996bb35edf545324ba012
panmrherobrine <polomarket73@gmail.com>
2018-12-05 15:06:44 +0100

    Dodano rysunek + nowe rozmieszczenie

--- scripts/script.py
+++ scripts/script.py
@@ -20,7 +20,7 @@ less15 = [
     b, w, w, w, w, w, w, b,
     b, w, w, w, w, w, w, b
 ]
-to38 = [
+to20 = [
     b, b, w, w, w, w, b, b,
     b, w, b, b, b, b, w, b,
     b, w, b, w, w, b, w, b,
@@ -30,7 +30,7 @@ to38 = [
     b, w, w, w, w, w, w, b,
     b, w, w, w, w, w, w, b
 ]
-wincyj39 = [
+wincyj25 = [
     b, b, w, w, w, w, b, b,
     b, w, b, b, b, b, w, b,
     b, w, b, w, w, b, w, b,
@@ -40,6 +40,7 @@ wincyj39 = [
     b, w, w, w, w, w, w, b,
     b, w, w, w, w, w, w, b
 ]
+
 
 sense.show_message("GREETINGS FROM POLAND", scroll_speed=0.07, text_colour=r) # POZDROWIENIA
 sleep(2)
@@ -47,10 +48,27 @@ sleep(2)
 
 if temp <= 15:
   sense.set_pixels(less15)
-elif temp >= 16 and temp <= 38:
+elif temp >= 16 and temp <= 20:
   sense.set_pixels(to38)
-elif temp >= 39:
-  sense.set_pixels(wincyj39)
+elif temp >= 21:
+    
+    S = (255, 208, 0) ## słońce
+    P = (255, 255, 66) ## piasek
+    N = (0, 174, 255) ## niebo
+    K = (0, 147, 38) ## kakktus
+    
+    wincyj39 = [
+        S, S, N, N, N, N, N, N,
+        S, N, N, N, N, K, N, N,
+        N, N, N, N, K, K, N, N,
+        N, N, N, N, N, K, K, N,
+        N, N, P, P, N, K, N, N,
+        N, P, P, P, P, P, P, P,
+        P, P, P, P, P, P, P, P,
+        P, P, P, P, P, P, P, P
+    ]
+    
+    sense.set_pixels(wincyj39)
 
 
 
